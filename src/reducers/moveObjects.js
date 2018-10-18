8e08b089ff2f5a3461caa118991f1a621b1f58d8
Victor <v.chumakovv@gmail.com>
2018-10-18 11:11:31 +0300

    fix moveObject

--- src/reducers/moveObjects.js
+++ src/reducers/moveObjects.js
@@ -8,6 +8,8 @@ function moveObjects(state, action) {
 
   let cannonBalls = moveBalls(state.gameState.cannonBalls);
 
+  
+
   const mousePosition = action.mousePosition || {
     x: 0,
     y: 0,
@@ -43,6 +45,7 @@ function moveObjects(state, action) {
   cannonBalls = cannonBalls.filter(cannonBall => (cannonBallsDestroyed.indexOf(cannonBall.id)));
   flyingObjects = flyingObjects.filter(flyingDisc => (flyingDiscsDestroyed.indexOf(flyingDisc.id)));
 
+  const kills = state.gameState.kills + flyingDiscsDestroyed.length;
   return {
     ...newState,
     gameState: {
@@ -51,6 +54,7 @@ function moveObjects(state, action) {
       cannonBalls: [...cannonBalls],
       lives,
       started,
+      kills,
     },
     angle,
   };
