312097d994e2028c6b06ebe9abb503d502ea4797
Victor <v.chumakovv@gmail.com>
2018-10-18 03:24:33 +0300

    add reducer for start game

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,24 @@
-import { MOVE_OBJECTS } from '../Actions';
+import { MOVE_OBJECTS, START_GAME } from '../Actions';
 import moveObjects from './moveObjects';
+import startGame from './startGame';
+
+const initialGameState = {
+  started: false,
+  kills: 0,
+  lives: 3,
+};
 
 const initialState = {
   angle: 45,
+  gameState: initialGameState,
 };
 
 function reducer(state = initialState, action) {
   switch (action.type) {
     case MOVE_OBJECTS:
       return moveObjects(state, action);
+    case START_GAME:
+      return startGame(state, initialGameState);
     default:
       return state;
   }
