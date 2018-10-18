0faf1badda6801fca60951d62fc42529915e0808
Victor <v.chumakovv@gmail.com>
2018-10-18 10:40:19 +0300

    Update src/components/Leaderboard.jsx

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -2,19 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Login from './Login';
 import Rank from "./Rank";
- const Leaderboard = (props) => {
+
+const Leaderboard = (props) => {
   const style = {
     fill: 'transparent',
     stroke: 'black',
     strokeDasharray: '15',
   };
-   const leaderboardTitle = {
+
+  const leaderboardTitle = {
     fontFamily: '"Joti One", cursive',
     fontSize: 50,
     fill: '#88da85',
     cursor: 'default',
   };
-   const leaderboard = props.leaderboard.sort((prev, next) => {
+
+  let leaderboard = props.leaderboard || [];
+  leaderboard = leaderboard.sort((prev, next) => {
     if (prev.maxScore === next.maxScore) {
       return prev.name <= next.name ? 1 : -1;
     }
@@ -22,32 +26,39 @@ import Rank from "./Rank";
   }).map((member, index) => ({
     ...member,
     rank: index + 1,
-    currentUser: member.id === props.currentUserId,
+    currentPlayer: member.id === props.currentPlayer.id,
   })).filter((member, index) => {
-    if (index < 3 || member.id === props.currentUserId) return member;
+    if (index < 3 || member.id === props.currentPlayer.id) return member;
     return null;
   });
-   return (
+
+  return (
     <g>
       <text filter="url(#shadow)" style={leaderboardTitle} x="-150" y="-630">Leaderboard</text>
       <rect style={style} x="-350" y="-600" width="700" height="330" />
       {
-        props.currentUserId && leaderboard.map((user, idx) => {
+        props.currentPlayer && leaderboard.map((player, idx) => {
           const position = {
             x: -100,
             y: -530 + (70 * idx)
           };
-          return <Rank key={user.id} user={user} rank={user.rank} position={position}/>
+          return <Rank key={player.id} player={player} position={position}/>
         })
       }
       {
-        ! props.currentUserId && <Login authenticate={props.authenticate} />
+        ! props.currentPlayer && <Login authenticate={props.authenticate} />
       }
     </g>
   );
 };
- Leaderboard.propTypes = {
-  currentUserId: PropTypes.string,
+
+Leaderboard.propTypes = {
+  currentPlayer: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    maxScore: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    picture: PropTypes.string.isRequired,
+  }),
   authenticate: PropTypes.func.isRequired,
   leaderboard: PropTypes.arrayOf(PropTypes.shape({
     id: PropTypes.string.isRequired,
@@ -55,9 +66,12 @@ import Rank from "./Rank";
     name: PropTypes.string.isRequired,
     picture: PropTypes.string.isRequired,
     ranking: PropTypes.number,
-  })).isRequired,
+  })),
 };
- Leaderboard.defaultProps = {
-  currentUserId: null
+
+Leaderboard.defaultProps = {
+  currentPlayer: null,
+  leaderboard: null,
 };
- export default Leaderboard;
+
+export default Leaderboard;
