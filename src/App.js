85ff2a41bd4d84da09d32a9a90dd0a7c1c6e42aa
Victor <v.chumakovv@gmail.com>
2018-10-18 09:55:50 +0300

    add auth

--- src/App.js
+++ src/App.js
@@ -2,10 +2,26 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import { getCanvasPosition } from './utils/formula';
 import Canvas from './components/Canvas';
+import * as Auth0 from 'auth0-web';
+
+Auth0.configure({
+  domain: 'chumakovv.auth0.com', // домен
+  clientID: 'ABU3Te2KdYYtRqHVPTZEJYODyNd6ERLm', // клиент id
+  redirectUri: 'http://localhost:3000/',
+  responseType: 'token id_token',
+  scope: 'openid profile manage:points',
+});
 
 class App extends Component {
   componentDidMount() {
     const self = this;
+
+    Auth0.handleAuthCallback();
+
+    Auth0.subscribe((auth) => {
+      console.log(auth);
+    });
+
     setInterval(() => {
         self.props.moveObjects(self.canvasMousePosition);
     }, 10);
